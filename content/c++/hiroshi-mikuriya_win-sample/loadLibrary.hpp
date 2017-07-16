15c91642be7ef030e28a50a34c4bbf39824a3ea5
hiroshi <hiroshi@iMacHiroshi2.local>
2017-07-16 22:43:26 +0900

    mod space

--- content/c++/hiroshi-mikuriya_win-sample/loadLibrary.hpp
+++ content/c++/hiroshi-mikuriya_win-sample/loadLibrary.hpp
@@ -16,16 +16,16 @@ ShowFirework_t * ShowFirework;
 
 inline void loadLibrary(const char * lib)
 {
-	HMODULE mod = LoadLibraryA(lib);
+    HMODULE mod = LoadLibraryA(lib);
     if(0 == mod){
         throw std::runtime_error("Failed to load library");
     }
-	SetUrl = reinterpret_cast<SetUrl_t*>(GetProcAddress(mod, "SetUrl"));
-	SetLed = reinterpret_cast<SetLed_t*>(GetProcAddress(mod, "SetLed"));
-	Clear = reinterpret_cast<Clear_t*>(GetProcAddress(mod, "Clear"));
-	Show = reinterpret_cast<Show_t*>(GetProcAddress(mod, "Show"));
-	Wait = reinterpret_cast<Wait_t*>(GetProcAddress(mod, "Wait"));
+    SetUrl = reinterpret_cast<SetUrl_t*>(GetProcAddress(mod, "SetUrl"));
+    SetLed = reinterpret_cast<SetLed_t*>(GetProcAddress(mod, "SetLed"));
+    Clear = reinterpret_cast<Clear_t*>(GetProcAddress(mod, "Clear"));
+    Show = reinterpret_cast<Show_t*>(GetProcAddress(mod, "Show"));
+    Wait = reinterpret_cast<Wait_t*>(GetProcAddress(mod, "Wait"));
     SetChar = reinterpret_cast<SetChar_t*>(GetProcAddress(mod, "SetChar"));
-	ShowMotioningText1 = reinterpret_cast<ShowMotioningText1_t*>(GetProcAddress(mod, "ShowMotioningText1"));
+    ShowMotioningText1 = reinterpret_cast<ShowMotioningText1_t*>(GetProcAddress(mod, "ShowMotioningText1"));
     ShowFirework = reinterpret_cast<ShowFirework_t*>(GetProcAddress(mod, "ShowFirework"));
 }
