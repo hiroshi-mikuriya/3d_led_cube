7fd2483ea120cd78c0a6135a7bef55c15b76268b
fxat00768 <hiroshi.mikuriya@fxat.co.jp>
2017-07-20 19:32:12 +0900

    mod algorithm

--- content/c++/takahiro-uchida_win-koekatamarin/main.cpp
+++ content/c++/takahiro-uchida_win-koekatamarin/main.cpp
@@ -1,6 +1,9 @@
 #include "loadLibrary.hpp"
 #include "GdiplusInitializer.hpp"
 #include "BitmapLockWrapper.hpp"
+#include <algorithm>
+#include <vector>
+#include <memory>
 #include <atlstr.h> 
 
 #pragma comment(lib, "gdiplus.lib")
@@ -10,26 +13,42 @@ namespace
 	void DrawChar(Gdiplus::Bitmap * canvas, WCHAR chr)
 	{
 		Gdiplus::Graphics g(canvas);
-		Gdiplus::Font font(L"Tahoma", canvas->GetWidth());
+		Gdiplus::Font font(L"メイリオ", canvas->GetWidth() / 2);
 		Gdiplus::SolidBrush brush(Gdiplus::Color(0xFF, 0xFF, 0xFF));
 		Gdiplus::RectF rc(0, 0, canvas->GetWidth(), canvas->GetHeight());
 		Gdiplus::StringFormat format;
 		g.DrawString(CString(chr), canvas->GetWidth(), &font, rc, &format, &brush);
 	}
 
+	void SetChar(int x, int y, int z, BitmapLockWrapper * lock)
+	{
+		for (int xx = 0; xx < LED_WIDTH; ++xx){
+			for (int yy = 0; yy < LED_WIDTH; ++yy){
+				int x2 = x + xx;
+				int y2 = y + yy;
+				byte v = *(static_cast<byte*>(lock->Get()->Scan0) + xx * 3 + yy * lock->Get()->Stride);
+				SetLed(x2, y2, z, v);
+			}
+		}
+	}
+
 	void ShowText(CString const & text)
 	{
-		for (int i = 0; i < text.GetLength(); ++i){
-			Gdiplus::Bitmap canvas(LED_WIDTH, LED_HEIGHT, PixelFormat24bppRGB);
-			DrawChar(&canvas, text[i]);
-			BitmapLockWrapper locker(&canvas);
-			Clear();
-			for (int x = 0; x < LED_WIDTH; ++x){
-				for (int y = 0; y < LED_HEIGHT; ++y){
-					byte v = *(static_cast<byte*>(locker.Get()->Scan0) + x * 3 + y * locker.Get()->Stride);
-					SetLed(x, y, 0, v);
-				}
+		typedef std::shared_ptr<Gdiplus::Bitmap> BitmapPtr;
+		typedef std::shared_ptr<BitmapLockWrapper> LockPtr;
+		auto imgs = [text](){
+			std::vector<std::pair<BitmapPtr, LockPtr>> dst;
+			for (int i = 0; i < text.GetLength(); ++i){
+				BitmapPtr bmp = std::make_shared<Gdiplus::Bitmap>(LED_WIDTH, LED_WIDTH, PixelFormat24bppRGB);
+				DrawChar(bmp.get(), text[i]);
+				LockPtr lock = std::make_shared<BitmapLockWrapper>(bmp.get());
+				dst.push_back({ bmp, lock });
 			}
+			return dst;
+		}();
+		for (size_t i = 0; i < imgs.size(); ++i){
+			Clear();
+			SetChar(0, 0, 0, imgs[i].second.get());
 			Show();
 			Wait(1000);
 		}
@@ -46,7 +65,7 @@ int main(int argc, const char* argv[])
 		SetUrl(argv[1]);
 	}
 	for (;;) {
-		CString text(_T("あいうえお"));
+		CString text(_T("オレの名前は内田だ"));
 		ShowText(text);
 		exit(0);
     }
