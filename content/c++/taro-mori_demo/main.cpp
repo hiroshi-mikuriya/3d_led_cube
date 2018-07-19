b682f1ea74d98c9d01ea439cd33c715338609fef
fxat00768 <hiroshi.mikuriya@fxat.co.jp>
2018-07-19 15:40:32 +0900

    mod main

--- content/c++/taro-mori_demo/main.cpp
+++ content/c++/taro-mori_demo/main.cpp
@@ -5,6 +5,7 @@
 #include<vector>
 #include <random>
 #include <cmath>
+#include <boost/program_options.hpp>
 
 int N = 100;
 int get_power(int ix)
@@ -12,9 +13,11 @@ int get_power(int ix)
     int fade = N / 5;
     if (ix < fade) {
         return ix * 255 / fade;
-    } else if (ix < N - fade) {
+    }
+    else if (ix < N - fade) {
         return 255;
-    } else {
+    }
+    else {
         return (N - ix) * 255 / fade;
     }
 }
@@ -38,7 +41,7 @@ void concentric(proc_t const & proc)
         for (int x = 0; x < LED_WIDTH; ++x) {
             for (int y = 0; y < LED_HEIGHT; ++y) {
                 for (int z = 0; z < LED_DEPTH; ++z) {
-                    double d = proc(x - cx, y - cy, z - cz, ix*1.0/N);
+                    double d = proc(x - cx, y - cy, z - cz, ix*1.0 / N);
                     int col0 = static_cast<int>(std::round(ix - d)) % 64;
                     int col = (col0 % 6) == 0 ? col0 % 7 + 1 : 0;
                     int r = (col & 1) ? power : 0;
@@ -118,7 +121,7 @@ xyz_t sphere_face(std::mt19937 & rng)
         double d2 = pos.x*pos.x + pos.y*pos.y + pos.z*pos.z;
         if (0.1<d2 && d2 < 1) {
             double d = 0.5;
-            return{ pos.x * d, pos.y * d+0.1, pos.z * d };
+            return{ pos.x * d, pos.y * d + 0.1, pos.z * d };
         }
     }
 }
@@ -126,8 +129,8 @@ xyz_t sphere_face(std::mt19937 & rng)
 bool can_show(xyz_t const & p)
 {
     return 0 <= p.x && p.x < LED_WIDTH
-    && 0 <= p.y && p.y < LED_HEIGHT
-    && 0 <= p.z && p.z < LED_DEPTH;
+        && 0 <= p.y && p.y < LED_HEIGHT
+        && 0 <= p.z && p.z < LED_DEPTH;
 }
 
 void color_cube()
@@ -141,14 +144,14 @@ void color_cube()
         };
         return c(x) * 0x10000 + c(y) * 0x100 + c(z) * 0x1;
     };
-    const int P = 90*10;
+    const int P = 90 * 10;
 #if 1
     for (int i = 0; i < P; ++i) {
         double k = 0.8;
         double x = ((rand() % 101) / 50.0 - 1);
         double y = ((rand() % 101) / 50.0 - 1);
         double z = ((rand() % 101) / 50.0 - 1);
-        points.push_back({ { k*x, k*y, k*z }, rgb(x,y,z) });
+        points.push_back({ { k*x, k*y, k*z }, rgb(x, y, z) });
     }
 #else
     for (int i = 0; i < P; ++i) {
@@ -157,9 +160,9 @@ void color_cube()
         for (int sig = 0; sig < 4; ++sig) {
             double s0 = sig & 1 ? -k : k;
             double s1 = sig & 2 ? -k : k;
-            points.push_back({ { s0, s1, t }, rgb(i)});
-            points.push_back({ { s0, t, s1 }, rgb(i)});
-            points.push_back({ { t, s0, s1 }, rgb(i)});
+            points.push_back({ { s0, s1, t }, rgb(i) });
+            points.push_back({ { s0, t, s1 }, rgb(i) });
+            points.push_back({ { t, s0, s1 }, rgb(i) });
         }
     }
 #endif
@@ -171,11 +174,11 @@ void color_cube()
             xyz_t p1 = {
                 p0.p.x * cos(t) + p0.p.z * sin(t),
                 p0.p.y,
-                - p0.p.x * sin(t) + p0.p.z * cos(t),
+                -p0.p.x * sin(t) + p0.p.z * cos(t),
             };
-            double zo = sin(6.28*i / N * 2)*0.3+1;
+            double zo = sin(6.28*i / N * 2)*0.3 + 1;
             if (N - 30 < i) {
-                zo *= (N - i)*1.0/30;
+                zo *= (N - i)*1.0 / 30;
             }
             if (i < 30) {
                 zo *= i*1.0 / 30;
@@ -209,14 +212,16 @@ void fireworks()
             int i = ix % 90;
             if (i < 30) {
                 return i * 255 / 30;
-            } else if (i < 60) {
+            }
+            else if (i < 60) {
                 return (60 - i) * 255 / 30;
-            } else {
+            }
+            else {
                 return 0;
             }
         };
         int n = std::floor(x * 1 * 90);
-        return red(n) * 0x10000 + red(n+30) * 0x100 + red(n+60) * 0x1;
+        return red(n) * 0x10000 + red(n + 30) * 0x100 + red(n + 60) * 0x1;
     };
     auto darken = [](int x)->int
     {
@@ -228,7 +233,7 @@ void fireworks()
     std::uniform_real_distribution<double> dist(0, 1);
     for (int ix = 0; ix < N; ++ix) {
         Clear();
-        if (ix % 20 == 0 && 20<N-ix) {
+        if (ix % 20 == 0 && 20<N - ix) {
             double cx = LED_WIDTH * dist(rng);
             double cy = LED_HEIGHT * dist(rng);
             double cz = LED_DEPTH * dist(rng);
@@ -237,13 +242,14 @@ void fireworks()
                 poss.push_back({ { cx, cy, cz }, rgb(vs.back().len()) });
             }
         }
-        for (size_t i = 0; i<poss.size() ; ++i ){
+        for (size_t i = 0; i<poss.size(); ++i){
             auto & p = poss[i];
             auto & v = vs[i];
             if (can_show(p.p)) {
                 SetLed(p.p.x, p.p.y, p.p.z, p.color);
-            } else {
-                
+            }
+            else {
+
             }
             p.p = p.p + v;
             p.color = darken(p.color);
@@ -273,7 +279,7 @@ void repbang()
     int N = 216;
     for (int i = 0; i < N; ++i){
         Clear();
-        double r = 1-std::cos(i*3.1416 / N * 8);
+        double r = 1 - std::cos(i*3.1416 / N * 8);
         double t = i*3.1416 / N * 16;
         for (auto & p : ps){
             xyz_t q = {
@@ -282,7 +288,7 @@ void repbang()
                 std::sin(t)*p.p.x + std::cos(t)*p.p.z,
             };
             xyz_t pos = q * r * LED_HEIGHT + xyz(LED_WIDTH, LED_HEIGHT, LED_DEPTH)*0.5;
-            
+
             if (can_show(pos)){
                 SetLed(pos.x, pos.y, pos.z, p.color);
             }
@@ -298,17 +304,17 @@ void spiral()
     struct rtyc_t { rty_t p; int c; };
     std::vector<rtyc_t> pcs;
     std::mt19937 rng;
-    
+
     for (int i = 0; i < 1000; ++i){
         double r = ((rng() % 1000) / 1000.0) * 0.8 + 0.2;
         double y = (rng() % 1000) / 1000.0;
         double t = y*3.1416 * 16;// rng() % 1000;
         double t0 = 3.1416 * 2 / 3;
         auto c = [t0](double t)->int {
-            double v = std::sin(t * 2 /3);
+            double v = std::sin(t * 2 / 3);
             return v < 0 ? 0 : v * 255;
         };
-        int col = c(t)+c(t+t0)*256+c(t+t0*2)*65536;
+        int col = c(t) + c(t + t0) * 256 + c(t + t0 * 2) * 65536;
         pcs.push_back({ { r, t, y }, col });
     }
     const int N = 200;
@@ -317,20 +323,20 @@ void spiral()
     const int D = 20;
     auto darken = [D](int x, int d)->int
     {
-        int r = ((x & 0xff0000) * d / D ) & 0xff0000;
-        int g = ((x & 0xff00) * d/ D) & 0xff00;
-        int b = ((x & 0xff) * d/ D) & 0xff;
+        int r = ((x & 0xff0000) * d / D) & 0xff0000;
+        int g = ((x & 0xff00) * d / D) & 0xff00;
+        int b = ((x & 0xff) * d / D) & 0xff;
         return r + g + b;
     };
     for (int i = 0; i < N; ++i){
         Clear();
         int d = i < D ? i : N < i + D ? N - i : D;
         for (auto & pc : pcs){
-            double y = int((pc.p.y) * LED_HEIGHT+N-i*U) % LED_HEIGHT;
-            double x = (pc.p.r * std::cos(pc.p.t + i*T)) * LED_WIDTH / 2 + LED_WIDTH/2;
-            double z = (pc.p.r * std::sin(pc.p.t + i*T)) * LED_WIDTH / 2 + LED_DEPTH/2;
+            double y = int((pc.p.y) * LED_HEIGHT + N - i*U) % LED_HEIGHT;
+            double x = (pc.p.r * std::cos(pc.p.t + i*T)) * LED_WIDTH / 2 + LED_WIDTH / 2;
+            double z = (pc.p.r * std::sin(pc.p.t + i*T)) * LED_WIDTH / 2 + LED_DEPTH / 2;
             if (can_show({ x, y, z })){
-                SetLed(x, y, z, darken( pc.c, d ));
+                SetLed(x, y, z, darken(pc.c, d));
             }
         }
         Show();
@@ -345,9 +351,9 @@ void brown()
     std::deque<pos_t> poses[3]{
         {{ LED_WIDTH / 2, LED_HEIGHT / 4, LED_DEPTH / 2 }},
         { { LED_WIDTH / 2, LED_HEIGHT / 2, LED_DEPTH / 2 } },
-        { { LED_WIDTH / 2, LED_HEIGHT*3 / 4, LED_DEPTH / 2 } }
+        { { LED_WIDTH / 2, LED_HEIGHT * 3 / 4, LED_DEPTH / 2 } }
     };
-    
+
     auto normalize = [](pos_t const & s) -> pos_t {
         return{
             (s.x + LED_WIDTH) % LED_WIDTH,
@@ -357,30 +363,32 @@ void brown()
     };
     auto move = [](pos_t const & s) -> pos_t {
         switch (rand() % 12) {
-            default:
-            case 0: return{ s.x + 1, s.y, s.z };
-            case 1: return{ s.x - 1, s.y, s.z };
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-                return{ s.x, s.y + 1, s.z };
-            case 6:
-            case 7:
-            case 8:
-            case 9:
-                return{ s.x, s.y - 1, s.z };
-            case 10: return{ s.x, s.y, s.z + 1 };
-            case 11: return{ s.x, s.y, s.z - 1 };
+        default:
+        case 0: return{ s.x + 1, s.y, s.z };
+        case 1: return{ s.x - 1, s.y, s.z };
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            return{ s.x, s.y + 1, s.z };
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+            return{ s.x, s.y - 1, s.z };
+        case 10: return{ s.x, s.y, s.z + 1 };
+        case 11: return{ s.x, s.y, s.z - 1 };
         }
     };
     auto red = [](int ix)->int {
         int i = ix % 90;
         if (i < 30) {
             return i * 255 / 30;
-        } else if (i < 60) {
+        }
+        else if (i < 60) {
             return (60 - i) * 255 / 30;
-        } else {
+        }
+        else {
             return 0;
         }
     };
@@ -430,7 +438,7 @@ void skewed_sphere()
         double s = std::sin(t*3.14*T);
         double c = std::cos(t*3.14*T);
         double dx = (dx0*c + dz0*s)*0.5;
-        double dz = (-dx0*s + dz0*c)*2;
+        double dz = (-dx0*s + dz0*c) * 2;
         return std::sqrt(dx*dx + dy*dy + dz*dz);
     });
 }
@@ -450,9 +458,9 @@ void gala()
             stars.push_back({ r * rr, tdist(rng), y * rr, static_cast<int>(rng() % 0x1000000) });
         }
         else {
-            double r = 0.3+4*std::abs(one(rng));
+            double r = 0.3 + 4 * std::abs(one(rng));
             double y = one(rng)*0.1;
-            stars.push_back({ r , tdist(rng), y , static_cast<int>(rng() % 0x1000000) });
+            stars.push_back({ r, tdist(rng), y, static_cast<int>(rng() % 0x1000000) });
         }
     }
     const int D = 20;
@@ -472,8 +480,8 @@ void gala()
             xyz_t q{
                 star.r * std::cos(star.t),
                 star.y,
-                star.r * std::sin(star.t)};
-            double T = 2, U=-4;
+                star.r * std::sin(star.t) };
+            double T = 2, U = -4;
             double x = std::cos(T)*q.x - std::sin(T)*q.y;
             double y = std::sin(T)*q.x + std::cos(T)*q.y;
             double z = q.z;
@@ -502,10 +510,10 @@ void balls()
     std::mt19937 rng;
     for (int i = 1; i < 7; ++i){
         ps.push_back({
-            {static_cast<double>(i & 1 ? 0 : LED_WIDTH),
-                static_cast<double>(i & 2 ? 0 : LED_HEIGHT),
-                static_cast<double>(i & 4 ? 0 : LED_DEPTH)},
-            {0,0,0},
+            { static_cast<double>(i & 1 ? 0 : LED_WIDTH),
+            static_cast<double>(i & 2 ? 0 : LED_HEIGHT),
+            static_cast<double>(i & 4 ? 0 : LED_DEPTH) },
+            { 0, 0, 0 },
             (i & 1 ? 0xff0000 : 0) + (i & 2 ? 0xff00 : 0) + (i & 4 ? 0xff : 0)
         });
     }
@@ -514,7 +522,7 @@ void balls()
         Clear();
         bool shown = false;
         for (auto & p : ps){
-            if (ix<N && ( !can_show(p.p) || p.v.len() == 0)){
+            if (ix<N && (!can_show(p.p) || p.v.len() == 0)){
                 xyz_t dir;
                 for (;;){
                     xyz_t dest{ static_cast<double>(rng() % LED_WIDTH), static_cast<double>(rng() % LED_HEIGHT), static_cast<double>(rng() % LED_DEPTH) };
@@ -523,7 +531,7 @@ void balls()
                         break;
                     }
                 }
-                p.v = dir *(1.0 / dir.len()*2);
+                p.v = dir *(1.0 / dir.len() * 2);
             }
             p.p += p.v;
             for (int i = 0; i < 125; ++i){
@@ -551,45 +559,45 @@ void balls()
 void threed()
 {
     char m0[] =
-    "00000000"/*0*/
-    "01111110"/*1*/
-    "01111110"/*2*/
-    "00000110"/*3*/
-    "00000110"/*4*/
-    "00000110"/*5*/
-    "00000110"/*6*/
-    "00111110"/*7*/
-    "00111110"/*8*/
-    "00000110"/*9*/
-    "00000110"/*a*/
-    "00000110"/*b*/
-    "00000110"/*c*/
-    "01111110"/*d*/
-    "01111110"/*e*/
-    "00000000"/*f*/;
+        "00000000"/*0*/
+        "01111110"/*1*/
+        "01111110"/*2*/
+        "00000110"/*3*/
+        "00000110"/*4*/
+        "00000110"/*5*/
+        "00000110"/*6*/
+        "00111110"/*7*/
+        "00111110"/*8*/
+        "00000110"/*9*/
+        "00000110"/*a*/
+        "00000110"/*b*/
+        "00000110"/*c*/
+        "01111110"/*d*/
+        "01111110"/*e*/
+        "00000000"/*f*/;
     char m1[] =
-    "00000000"/*0*/
-    "01111000"/*1*/
-    "01111100"/*2*/
-    "01101110"/*3*/
-    "01100110"/*4*/
-    "01100110"/*5*/
-    "01100110"/*6*/
-    "01100110"/*7*/
-    "01100110"/*8*/
-    "01100110"/*9*/
-    "01100110"/*a*/
-    "01100110"/*b*/
-    "01101110"/*c*/
-    "01111100"/*d*/
-    "01111000"/*e*/
-    "00000000"/*f*/;
+        "00000000"/*0*/
+        "01111000"/*1*/
+        "01111100"/*2*/
+        "01101110"/*3*/
+        "01100110"/*4*/
+        "01100110"/*5*/
+        "01100110"/*6*/
+        "01100110"/*7*/
+        "01100110"/*8*/
+        "01100110"/*9*/
+        "01100110"/*a*/
+        "01100110"/*b*/
+        "01101110"/*c*/
+        "01111100"/*d*/
+        "01111000"/*e*/
+        "00000000"/*f*/;
     std::mt19937 rng;
     auto col_at = [&m0, &m1, &rng](int x, int y, int c)->int {
         char const * m = c == 0 ? m0 : m1;
         return m[x / 2 + (y / 2) * 8] == '0' ? 0 :
-        c == 0 ? x * 0x100000 + (16 - x) * 0x1000
-        : y * 0x8 + (31 - y) * 0x80000;
+            c == 0 ? x * 0x100000 + (16 - x) * 0x1000
+            : y * 0x8 + (31 - y) * 0x80000;
     };
     auto col_at2 = [&col_at](xyz_t const & p, int c)->int {
         return col_at(p.x, p.y, c);
@@ -607,23 +615,23 @@ void threed()
             static_cast<double>(rng() % LED_WIDTH),
             static_cast<double>(rng() % LED_HEIGHT),
             static_cast<double>(rng() % LED_DEPTH) };
-        if (col_at2(a,0) && col_at2(b,1)) {
+        if (col_at2(a, 0) && col_at2(b, 1)) {
             pps.push_back({ { a, b } });
         }
     }
     int N0 = 20;
     int N1 = 20;
-    auto show = [&col_at2,&pps, N0](double c, double d)->void {
+    auto show = [&col_at2, &pps, N0](double c, double d)->void {
         Clear();
         for (auto & pp : pps) {
-            xyz_t p = pp.p[0] * (1-c) + pp.p[1] * c;
+            xyz_t p = pp.p[0] * (1 - c) + pp.p[1] * c;
             int c0 = col_at2(pp.p[0], 0);
             int c1 = col_at2(pp.p[1], 1);
             auto mean = [&](int mask, double d) {
                 int v = ((c0 & mask)*(1 - c) + (c1 & mask)*c)*d;
                 return v & mask;
             };
-            int col = mean(0xff0000,d) + mean(0xff00,d) + mean(0xff,d);
+            int col = mean(0xff0000, d) + mean(0xff00, d) + mean(0xff, d);
             if (can_show(p)) {
                 SetLed(p.x, p.y, p.z, col);
             }
@@ -632,12 +640,12 @@ void threed()
         Wait(50);
     };
     for (int ix = 0; ix < N0; ++ix) {
-        show(0, ix*1.0/N0);
+        show(0, ix*1.0 / N0);
     }
     for (int ix = 0; ix < N1; ++ix) {
-        show(ix*1.0/N1, 1);
+        show(ix*1.0 / N1, 1);
     }
-    for (int ix = 0; ix < N0/2; ++ix) {
+    for (int ix = 0; ix < N0 / 2; ++ix) {
         show(1, 1);
     }
     for (int ix = 0; ix < N0; ++ix) {
@@ -667,22 +675,22 @@ void wave()
         double light = i < LT ? i / LT : N0 - i<LT ? (N0 - i) / LT : 1;
         for (int iy = 0; iy < LED_HEIGHT; ++iy){
             for (int ix = 0; ix < LED_WIDTH; ++ix){
-                double x = xt*(ix-LED_WIDTH/2)/LED_WIDTH*2;
-                double y = yt*(iy - LED_HEIGHT / 2) / LED_HEIGHT*2;
-                double iz = static_cast<int>(sin(x)*sin(y)*sin(i*it)*LED_DEPTH/2+ LED_DEPTH/2);
+                double x = xt*(ix - LED_WIDTH / 2) / LED_WIDTH * 2;
+                double y = yt*(iy - LED_HEIGHT / 2) / LED_HEIGHT * 2;
+                double iz = static_cast<int>(sin(x)*sin(y)*sin(i*it)*LED_DEPTH / 2 + LED_DEPTH / 2);
                 auto col = [&](double t)->int{
                     double dx = (ix - LED_WIDTH / 2);
                     double dy = (iy - LED_HEIGHT / 2);
                     double d = std::pow(dx*dx + dy*dy, 0.7) / 10.0 + t + i*it;
                     double W = sin(3.1416 * 7 / 6);
-                    double v = sin(d) < W ? 0 : (sin(d)-W) / (1-W);
-                    return 255 & int(v * 255*light);
+                    double v = sin(d) < W ? 0 : (sin(d) - W) / (1 - W);
+                    return 255 & int(v * 255 * light);
                 };
                 const double T = 3.1416 * 2 / 3;
-                int c = col(T) * 1 + col(T*2) * 0x100 + col(T*3) * 0x10000;
+                int c = col(T) * 1 + col(T * 2) * 0x100 + col(T * 3) * 0x10000;
                 for (int dz : { -1, 0, 1 }){
-                    if (can_show({ static_cast<double>(ix), static_cast<double>(iy), iz+dz })){
-                        SetLed(ix, iy, iz+dz, c);
+                    if (can_show({ static_cast<double>(ix), static_cast<double>(iy), iz + dz })){
+                        SetLed(ix, iy, iz + dz, c);
                     }
                 }
             }
@@ -699,34 +707,60 @@ struct yz_t
 
 int main(int argc, const char* argv[])
 {
-    static_cast<void>(argc); // unused
-    static_cast<void>(argv); // unused
+    namespace po = boost::program_options;
+    po::options_description desc("3D LED DEMO PROGRAM");
+    desc.add_options()	//
+        ("help,h", "produce help message")	//
+        ("version,v", "produce version information")	//
+        ("url,u", po::value<std::string>(), "3D LED URL")	//
+        ("port,p", po::value<int>(), "3D LED PORT")	//
+        ;
+    po::variables_map vm;
+    try
+    {
 #if defined(WIN32)
-	loadLibrary("ledLib32.dll");
+        loadLibrary("ledLib32.dll");
 #elif defined(WIN64)
-    loadLibrary("ledLib64.dll");
-#elif defined(__APPLE__)
-    loadLibrary("./libledLib.dylib");
+        loadLibrary("ledLib64.dll");
 #endif
-	if (1 < argc){
-		SetUrl(argv[1]);
-	}
-    if(2 < argc){
-        SetPort(atoi(argv[2]));
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        if (vm.count("version"))
+        {
+            std::cout << "version 1. 2018. 0" << std::endl;
+            return 0;
+        }
+        if (vm.count("help"))
+        {
+            std::cout << desc << std::endl;
+            return 0;
+        }
+        po::notify(vm);
+        if (0 < vm.count("url") && 0 < vm.count("port")){
+            std::string const url = vm["url"].as<std::string>();
+            int const port = vm["port"].as<int>();
+            std::cout << "URL : " << url << "\n" "PORT : " << port << std::endl;
+            SetUrl(url.c_str());
+            SetPort(port);
+        }
     }
-	for (;;) {
-		ShowMotioningText1("MAKERFAIRE2018");
+    catch (std::exception const & e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+    for (;;) {
+        ShowMotioningText1("MAKERFAIRE2018");
         wave();
         balls();
         gala();
         skewed_sphere();
         threed();
         repbang();
-        color_cube ();
+        color_cube();
         spiral();
         cube();
         fireworks();
         brown();
         sphere();
     }
+    return 0; // unreachable
 }
